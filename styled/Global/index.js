854ac24c690ce1d398405c8d47e4263bb82898f8
Krista Handel <k.handel66@gmail.com>
2019-03-11 16:55:31 -0600

    misc/run linter and fix

--- styled/Global/index.js
+++ styled/Global/index.js
@@ -1,15 +1,19 @@
 export default () => {
-	return (
-		<style global jsx>{`
-			@import url('https://fonts.googleapis.com/css?family=Crimson+Text');
-			body {
-				font-family: 'Crimson Text', serif;
-				margin: 0;
-				padding: 0;
-				background: #FCFCFC;
-				color: #3B3F4D;
-			}
-		`}
-		</style>
-		)
+  return (
+    <style global jsx>{`
+      @import url('https://fonts.googleapis.com/css?family=Crimson+Text');
+      @import url('https://fonts.googleapis.com/css?family=Inconsolata');
+      body {
+        font-family: 'Inconsolata', monospace;
+        margin: 0;
+        padding: 0;
+        background: #FCFCFC;
+        color: #3B3F4D;
+      }
+      p {
+        font-family: 'Crimson Text', sans-serif;
+      }
+    `}
+    </style>
+  )
 }
