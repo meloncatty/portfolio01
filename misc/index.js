501907c2bda2ff64d59b017e418ea37f9178746a
Krista Handel <k.handel66@gmail.com>
2019-03-11 16:57:37 -0600

    styles/add css pixel art

---
+++ misc/index.js
@@ -0,0 +1,27 @@
+export const cssCat = <div style={{
+  boxShadow: '6px 2px 0 0 #000000, 16px 2px 0 0 #000000, 4px 4px 0 0 #000000, 6px 4px 0 0 #2196f3, 8px 4px 0 0 #000000, 14px 4px 0 0 #000000, 16px 4px 0 0 #2196f3, 18px 4px 0 0 #000000, 4px 6px 0 0 #000000, 6px 6px 0 0 #2196f3, 8px 6px 0 0 #2196f3, 10px 6px 0 0 #000000, 12px 6px 0 0 #000000, 14px 6px 0 0 #2196f3, 16px 6px 0 0 #2196f3, 18px 6px 0 0 #000000, 2px 8px 0 0 #000000, 4px 8px 0 0 #2196f3, 6px 8px 0 0 #2196f3, 8px 8px 0 0 #2196f3, 10px 8px 0 0 #2196f3, 12px 8px 0 0 #2196f3, 14px 8px 0 0 #2196f3, 16px 8px 0 0 #2196f3, 18px 8px 0 0 #2196f3, 20px 8px 0 0 #000000, 2px 10px 0 0 #000000, 4px 10px 0 0 #2196f3, 6px 10px 0 0 #2196f3, 8px 10px 0 0 #000000, 10px 10px 0 0 #2196f3, 12px 10px 0 0 #2196f3, 14px 10px 0 0 #000000, 16px 10px 0 0 #2196f3, 18px 10px 0 0 #2196f3, 20px 10px 0 0 #000000, 2px 12px 0 0 #000000, 4px 12px 0 0 #2196f3, 6px 12px 0 0 #2196f3, 8px 12px 0 0 #000000, 10px 12px 0 0 #2196f3, 12px 12px 0 0 #2196f3, 14px 12px 0 0 #000000, 16px 12px 0 0 #2196f3, 18px 12px 0 0 #2196f3, 20px 12px 0 0 #000000, 2px 14px 0 0 #000000, 4px 14px 0 0 #2196f3, 6px 14px 0 0 #e2abb1, 8px 14px 0 0 #2196f3, 10px 14px 0 0 #2196f3, 12px 14px 0 0 #2196f3, 14px 14px 0 0 #2196f3, 16px 14px 0 0 #e2abb1, 18px 14px 0 0 #2196f3, 20px 14px 0 0 #000000, 4px 16px 0 0 #000000, 6px 16px 0 0 #03a9f4, 8px 16px 0 0 #2196f3, 10px 16px 0 0 #2196f3, 12px 16px 0 0 #2196f3, 14px 16px 0 0 #2196f3, 16px 16px 0 0 #03a9f4, 18px 16px 0 0 #000000, 6px 18px 0 0 #000000, 8px 18px 0 0 #03a9f4, 10px 18px 0 0 #03a9f4, 12px 18px 0 0 #03a9f4, 14px 18px 0 0 #03a9f4, 16px 18px 0 0 #000000',
+  height: '2px',
+  margin: '2px 0 0 5px',
+  width: '2px'
+}} />
+
+export const cssStar = <div style={{
+  boxShadow: '10px 2px 0 0 #000000, 8px 4px 0 0 #000000, 10px 4px 0 0 #ffeb3b, 12px 4px 0 0 #000000, 8px 6px 0 0 #000000, 10px 6px 0 0 #ffeb3b, 12px 6px 0 0 #000000, 4px 8px 0 0 #000000, 6px 8px 0 0 #000000, 8px 8px 0 0 #ffeb3b, 10px 8px 0 0 #ffeb3b, 12px 8px 0 0 #ffeb3b, 14px 8px 0 0 #000000, 16px 8px 0 0 #000000, 2px 10px 0 0 #000000, 4px 10px 0 0 #ffeb3b, 6px 10px 0 0 #ffeb3b, 8px 10px 0 0 #ffeb3b, 10px 10px 0 0 #ffeb3b, 12px 10px 0 0 #ffeb3b, 14px 10px 0 0 #ffeb3b, 16px 10px 0 0 #ffeb3b, 18px 10px 0 0 #000000, 4px 12px 0 0 #000000, 6px 12px 0 0 #000000, 8px 12px 0 0 #ffeb3b, 10px 12px 0 0 #ffeb3b, 12px 12px 0 0 #ffeb3b, 14px 12px 0 0 #000000, 16px 12px 0 0 #000000, 8px 14px 0 0 #000000, 10px 14px 0 0 #ffeb3b, 12px 14px 0 0 #000000, 8px 16px 0 0 #000000, 10px 16px 0 0 #ffeb3b, 12px 16px 0 0 #000000, 10px 18px 0 0 #000000',
+  height: '2px',
+  margin: '10px 0 0 5px',
+  width: '2px'
+}} />
+
+export const cssHeart = <div style={{
+  boxShadow: '4px 2px 0 0 #000000, 6px 2px 0 0 #000000, 10px 2px 0 0 #000000, 12px 2px 0 0 #000000, 2px 4px 0 0 #000000, 4px 4px 0 0 #e91e63, 6px 4px 0 0 #e91e63, 8px 4px 0 0 #000000, 10px 4px 0 0 #e91e63, 12px 4px 0 0 #e91e63, 14px 4px 0 0 #000000, 2px 6px 0 0 #000000, 4px 6px 0 0 #e91e63, 6px 6px 0 0 #e91e63, 8px 6px 0 0 #e91e63, 10px 6px 0 0 #e91e63, 12px 6px 0 0 #e91e63, 14px 6px 0 0 #000000, 2px 8px 0 0 #000000, 4px 8px 0 0 #e91e63, 6px 8px 0 0 #e91e63, 8px 8px 0 0 #e91e63, 10px 8px 0 0 #e91e63, 12px 8px 0 0 #e91e63, 14px 8px 0 0 #000000, 4px 10px 0 0 #000000, 6px 10px 0 0 #e91e63, 8px 10px 0 0 #e91e63, 10px 10px 0 0 #e91e63, 12px 10px 0 0 #000000, 6px 12px 0 0 #000000, 8px 12px 0 0 #e91e63, 10px 12px 0 0 #000000, 8px 14px 0 0 #000000',
+  height: '2px',
+  margin: '12px 0 0 5px',
+  width: '2px'
+}} />
+
+export const cssLeaf = <div style={{
+  boxShadow: '4px 4px 0 0 #000000, 6px 4px 0 0 #000000, 14px 4px 0 0 #000000, 16px 4px 0 0 #000000, 2px 6px 0 0 #000000, 4px 6px 0 0 #8bc34a, 6px 6px 0 0 #8bc34a, 8px 6px 0 0 #000000, 12px 6px 0 0 #000000, 14px 6px 0 0 #8bc34a, 16px 6px 0 0 #8bc34a, 18px 6px 0 0 #000000, 2px 8px 0 0 #000000, 4px 8px 0 0 #8bc34a, 6px 8px 0 0 #8bc34a, 8px 8px 0 0 #9ee07a, 10px 8px 0 0 #000000, 12px 8px 0 0 #9ee07a, 14px 8px 0 0 #8bc34a, 16px 8px 0 0 #8bc34a, 18px 8px 0 0 #000000, 4px 10px 0 0 #000000, 6px 10px 0 0 #000000, 8px 10px 0 0 #000000, 10px 10px 0 0 #9ee07a, 12px 10px 0 0 #000000, 14px 10px 0 0 #000000, 16px 10px 0 0 #000000, 10px 12px 0 0 #000000, 10px 14px 0 0 #000000, 10px 16px 0 0 #000000, 2px 18px 0 0 #000000, 8px 18px 0 0 #000000, 4px 20px 0 0 #000000, 6px 20px 0 0 #000000',
+  height: '2px',
+  width: '2px',
+  margin: '6px 0 0 5px'
+}} />
